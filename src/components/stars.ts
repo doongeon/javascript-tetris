ab778d885e80deda2363c87c127ec9b945c9ab14
donggeon Na <ndmb2012@gmail.com>
2024-06-25 17:53:45 +0900

    create: function render stars

---
+++ src/components/stars.ts
@@ -0,0 +1,12 @@
+export function renderStars(n: number) {
+  const gameBackground = document.getElementById("gameBackground");
+  [...new Array(n)].map((__) => {
+    const star = document.createElement("div");
+    star.classList.add("star");
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.animationDelay = `${Math.random() * 2}s`;
+
+    gameBackground?.appendChild(star);
+  });
+}
