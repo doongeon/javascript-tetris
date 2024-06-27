acbfd5aa90b30fcec6935efb071bdd5795326e06
donggeon Na <ndmb2012@gmail.com>
2024-06-27 18:12:54 +0900

    update: 스핀 기술을 위한 로직 추가, BeepSound 클래스 사용법 변경

--- src/classes/Service.ts
+++ src/classes/Service.ts
@@ -16,7 +16,6 @@ export class Service {
   isStart = false;
   winScore: number;
   score: number;
-  beepSound = new BeepSound();
   callback: ({ score, isWin }: { score: number; isWin: boolean }) => void;
 
   constructor(
@@ -37,16 +36,17 @@ export class Service {
     this.callback = callback;
   }
 
-  start() {
+  public start() {
     this.startDrawInterval();
-    this.isStart = true;
     this.fallingDownInterval = this.getFallingDownInterval();
+    this.isStart = true;
     View.drawPreview(this.tetrisBlocks[0]);
   }
 
-  dropBlock() {
+  public dropBlock() {
     if (!this.isStart) return;
-    BeepSound.beepMove();
+
+    BeepSound.beepDrop();
     while (
       !CollisionDetector.detectCollisionOnBottom(
         this.grid,
@@ -61,9 +61,8 @@ export class Service {
     this.next();
   }
 
-  moveBlockLeft() {
+  public moveBlockLeft() {
     if (!this.isStart) return;
-
     if (
       CollisionDetector.detectCollisionOnLeft(this.grid, this.movingTetrisBlock)
     ) {
@@ -77,9 +76,8 @@ export class Service {
     this.grid.drawBlock(this.movingTetrisBlock);
   }
 
-  moveBlockRight() {
+  public moveBlockRight() {
     if (!this.isStart) return;
-
     if (
       CollisionDetector.detectCollisionOnRight(
         this.grid,
@@ -96,9 +94,8 @@ export class Service {
     this.grid.drawBlock(this.movingTetrisBlock);
   }
 
-  moveBlockDown() {
+  public moveBlockDown() {
     if (!this.isStart) return;
-
     if (
       CollisionDetector.detectCollisionOnBottom(
         this.grid,
@@ -115,6 +112,17 @@ export class Service {
     this.grid.drawBlock(this.movingTetrisBlock);
   }
 
+  /**
+   * 1. 움직이는 블록을 그리드에서 지운다.
+   * 2. 움직이는 블록을 지운 그리드의 복사본을 생성한다.
+   * 3. 움직이는 블록의 복사본을 생성한다.
+   * 4. 블록의 복사본을 이동 시킨후 회전하여 충동이 있는지 확인한다.
+   * 5. 충돌이 있는지 확인은 그리드 복사본에서 한다.
+   * 6. 복사복에서 충돌이 없는 경우 원래 블록을 같은 방식으로 이동시킨후 회전한다.
+   * 7. 원래 그리드에서 그린다.
+   *
+   *  블록 복사본을 이동하는 경우마다 주석을 적기.
+   */
   rotateBlock() {
     if (!this.isStart) return;
 
@@ -156,38 +164,6 @@ export class Service {
       !CollisionDetector.checkCollision(tetrisBlockCopy.getCells(), this.grid)
     ) {
       this.movingTetrisBlock.moveLeft();
-      this.movingTetrisBlock.rotate();
-      this.grid.drawBlock(this.movingTetrisBlock);
-      return;
-    }
-
-    // 왼쪽 한번 위쪽 한번 이동후 회전 충돌 확인
-    tetrisBlockCopy = this.movingTetrisBlock.getDeepCopy();
-    tetrisBlockCopy.moveLeft();
-    tetrisBlockCopy.moveUp();
-    tetrisBlockCopy.rotate();
-
-    if (
-      !CollisionDetector.checkCollision(tetrisBlockCopy.getCells(), this.grid)
-    ) {
-      this.movingTetrisBlock.moveLeft();
-      this.movingTetrisBlock.moveUp();
-      this.movingTetrisBlock.rotate();
-      this.grid.drawBlock(this.movingTetrisBlock);
-      return;
-    }
-
-    //오른쪽 한번 휘쪽 한번 이동후 회전 충돌 확인
-    tetrisBlockCopy = this.movingTetrisBlock.getDeepCopy();
-    tetrisBlockCopy.moveRight();
-    tetrisBlockCopy.moveUp();
-    tetrisBlockCopy.rotate();
-
-    if (
-      !CollisionDetector.checkCollision(tetrisBlockCopy.getCells(), this.grid)
-    ) {
-      this.movingTetrisBlock.moveRight();
-      this.movingTetrisBlock.moveUp();
       this.movingTetrisBlock.rotate();
       this.grid.drawBlock(this.movingTetrisBlock);
       return;
@@ -239,7 +215,7 @@ export class Service {
       return;
     }
 
-    //아래 한번 왼쪽 한번 이동후 회전 충돌 확인
+    //아래 한번 왼쪽 한번 이동후 회전 충돌 확인\
     tetrisBlockCopy = this.movingTetrisBlock.getDeepCopy();
     tetrisBlockCopy.moveDown();
     tetrisBlockCopy.moveLeft();
@@ -307,31 +283,52 @@ export class Service {
       return;
     }
 
+    //아래 두번 이동후 회전 충돌 확인
+    tetrisBlockCopy = this.movingTetrisBlock.getDeepCopy();
+    tetrisBlockCopy.moveDown();
+    tetrisBlockCopy.moveDown();
+    tetrisBlockCopy.rotate();
+
+    if (
+      !CollisionDetector.checkCollision(tetrisBlockCopy.getCells(), this.grid)
+    ) {
+      this.movingTetrisBlock.moveDown();
+      this.movingTetrisBlock.moveDown();
+      this.movingTetrisBlock.rotate();
+      this.grid.drawBlock(this.movingTetrisBlock);
+      return;
+    }
+
+    //아래 두번 왼쪽 한번 이동후 회전 충돌 확인
+    tetrisBlockCopy = this.movingTetrisBlock.getDeepCopy();
+    tetrisBlockCopy.moveDown();
+    tetrisBlockCopy.moveDown();
+    tetrisBlockCopy.moveLeft();
+    tetrisBlockCopy.rotate();
+
+    if (
+      !CollisionDetector.checkCollision(tetrisBlockCopy.getCells(), this.grid)
+    ) {
+      this.movingTetrisBlock.moveDown();
+      this.movingTetrisBlock.moveDown();
+      this.movingTetrisBlock.moveLeft();
+      this.movingTetrisBlock.rotate();
+      this.grid.drawBlock(this.movingTetrisBlock);
+      return;
+    }
+
     // 회전 불가능일시
-    BeepSound.beepDeny();
     this.grid.drawBlock(this.movingTetrisBlock);
+    BeepSound.beepDeny();
     return;
   }
 
-  private getNewTetrisBlock() {
-    return TetrisBlockGenerator.getNewBlock();
-  }
-
-  private startDrawInterval() {
-    this.drawInterval = setInterval(() => {
-      View.draw(this.grid);
-    }, 16);
-  }
-
-  private clearDrawInterval() {
-    clearInterval(this.drawInterval);
-  }
-
-  private clearFallingDownInterval() {
-    this.isHitBottom = false;
-    clearInterval(this.fallingDownInterval);
-  }
-
+  /**
+   * this.dropDownIntervalTime 마다 블록을 하래로 한칸 내린다.
+   * 더이상 아래로 이동할 수 없는 경우 isHitBottom은 true으로 세팅.
+   * 다음 인터벌에도 아래로 이동할 수 없는 경우 다음 블럭으로 진행.
+   * 다음 인터벌에 아래로 이동할 수 있는경우 isHitBottom은 false으로 세팅.
+   */
   private getFallingDownInterval() {
     const fallingDownInterval = setInterval(() => {
       if (
@@ -364,14 +361,19 @@ export class Service {
     return fallingDownInterval;
   }
 
+  /**
+   * 다음 블럭으로 이동할때 항상 호출하는 로직
+   *
+   */
   private next() {
-    this.score += 100;
+    this.isStart = false; // 다음 블록이 설정 된 후에 컨트롤이 가능하도록 함
+    this.score += 5;
     this.clearFallingDownInterval();
     this.score += this.grid.clearRow();
     View.draw(this.grid);
     View.writeScore(this.score);
-
     console.log(this.score, this.winScore);
+
     if (this.score >= this.winScore) {
       console.log("win!");
       this.win();
@@ -380,6 +382,8 @@ export class Service {
 
     this.setNextBlock();
 
+    // 패배 조건
+    // 새로 나온 블럭과 기존 블록이 충돌할 때
     if (
       CollisionDetector.detectCollisionOnStart(
         this.grid,
@@ -393,6 +397,26 @@ export class Service {
 
     this.grid.drawBlock(this.movingTetrisBlock);
     this.fallingDownInterval = this.getFallingDownInterval();
+    this.isStart = true; // 다음 블록이 설정 된 후에 컨트롤이 가능하도록 함
+  }
+
+  private getNewTetrisBlock() {
+    return TetrisBlockGenerator.getNewBlock();
+  }
+
+  private startDrawInterval() {
+    this.drawInterval = setInterval(() => {
+      View.draw(this.grid);
+    }, 16);
+  }
+
+  private clearDrawInterval() {
+    clearInterval(this.drawInterval);
+  }
+
+  private clearFallingDownInterval() {
+    this.isHitBottom = false;
+    clearInterval(this.fallingDownInterval);
   }
 
   private setNextBlock() {
