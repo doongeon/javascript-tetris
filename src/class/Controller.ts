41a1934a0723d0749133a3300d76b0a2cfc98b78
donggeon Na <ndmb2012@gmail.com>
2024-07-05 16:49:30 +0900

    fix: page scroll

--- src/class/Controller.ts
+++ src/class/Controller.ts
@@ -12,18 +12,23 @@ export default class Controller {
   private handleKeyDown(event: KeyboardEvent) {
     switch (event.key) {
       case "ArrowLeft":
+        event.preventDefault();
         this.service.moveBlockLeft();
         break;
       case "ArrowRight":
+        event.preventDefault();
         this.service.moveBlockRight();
         break;
       case "ArrowDown":
+        event.preventDefault();
         this.service.moveBlockDown();
         break;
       case "ArrowUp":
+        event.preventDefault();
         this.service.rotateBlock();
         break;
       case " ":
+        event.preventDefault();
         this.service.dropBlock();
         break;
     }
