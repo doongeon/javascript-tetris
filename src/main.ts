3557c812bd59233bc830685633b932d6634f5ffa
donggeon Na <ndmb2012@gmail.com>
2024-07-05 16:11:43 +0900

    fix: iframe cors

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,15 @@
 import "./main.css";
 import { renderStars, renderCells, renderStartBtn } from "./components";
 
-const handleKeyDown = (event: KeyboardEvent) => {
-  const { key } = event;
-  if (
-    key === "ArrowUp" ||
-    key === "ArrowDown" ||
-    key === "ArrowLeft" ||
-    key === "ArrowRight" ||
-    key === " "
-  ) {
-    event.preventDefault();
-  }
-};
+// Add event listener for keydown event inside iframe
+document.addEventListener("keydown", function (event: KeyboardEvent) {
+  const keysToBlock = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", " "];
 
-window.addEventListener("keydown", handleKeyDown);
+  if (keysToBlock.includes(event.key)) {
+    // Send message to parent window to block scrolling
+    window.parent.postMessage("block-keys", "http://localhost:5173");
+  }
+});
 
 renderStars(100);
 renderCells();
