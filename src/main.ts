1bb8055a1f44faa5ccd63a53286869ade5700901
donggeon Na <ndmb2012@gmail.com>
2024-07-05 14:51:20 +0900

    fix: prevent scrolling on key down

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import "./main.css";
 import { renderStars, renderCells, renderStartBtn } from "./components";
 
+const handleKeyDown = (event: KeyboardEvent) => {
+  const { key } = event;
+  if (
+    key === "ArrowUp" ||
+    key === "ArrowDown" ||
+    key === "ArrowLeft" ||
+    key === "ArrowRight" ||
+    key === " "
+  ) {
+    event.preventDefault();
+  }
+};
+
+window.addEventListener("keydown", handleKeyDown);
+
 renderStars(100);
 renderCells();
 renderStartBtn();
