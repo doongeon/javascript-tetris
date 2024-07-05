e925be62e0d0566926f15d551074104da0068c14
donggeon Na <ndmb2012@gmail.com>
2024-07-05 16:52:40 +0900

    fix: prevent scroll on press arrow button

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,6 @@
 import "./main.css";
 import { renderStars, renderCells, renderStartBtn } from "./components";
 
-// Add event listener for keydown event inside iframe
-document.addEventListener("keydown", function (event: KeyboardEvent) {
-  const keysToBlock = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", " "];
-
-  if (keysToBlock.includes(event.key)) {
-    // Send message to parent window to block scrolling
-    window.parent.postMessage(
-      "block-keys",
-      "https://df64ytwdcjyj9.cloudfront.net"
-    );
-    console.log("scroll");
-  }
-});
-
 renderStars(100);
 renderCells();
 renderStartBtn();
